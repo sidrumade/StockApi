a3b92c1ed79484a27b7c3160f0e3f973c11605c8
siddhesh rumade <siddhesh.rumade.5@gmail.com>
2021-06-16 23:45:51 +0530

    Update forms.py
    Length changed

--- first_app/forms.py
+++ first_app/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class StockSymbolForm(forms.Form):
-    symbol = forms.CharField(max_length=4,min_length=4,strip=True,widget=forms.widgets.TextInput(
+    symbol = forms.CharField(max_length=6,min_length=1,strip=True,widget=forms.widgets.TextInput(
         attrs={'class': 'form-control',
                'placeholder':'GOOG'}
     ))
